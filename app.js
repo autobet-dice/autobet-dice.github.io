310c2ee87f72a9bef811564b8496f0162b77358b
grabby7 <cheetith@gmail.com>
2015-09-06 23:18:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -539,7 +539,7 @@ var betStore = new Store('bet', {
     }
 
     // Ensure wagerString is a number
-    if (isNaN(n) || /[^\d]/.test(n.toString())) {
+    if (IsNumeric(n) || /[^\d]/.test(n.toString())) {
       self.state.wager.error = 'INVALID_WAGER';
     // Ensure user can afford balance
     } else if (n * 100 > worldStore.state.user.balance) {
